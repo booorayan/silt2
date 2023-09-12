e0d1202ad30626a4022eb36f36d095eff130dac7
booorayan <boorayo@yahoo.com>
2023-09-12 15:54:38 +0300

    add phone number field

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 # Create your models here.
@@ -6,6 +7,7 @@ class Customer(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
     code = models.CharField(max_length=10)
+    phone_number = PhoneNumberField(blank=True)
 
     def __str__(self):
         return self.name
